2a250cb0aaee0d5151658daf9bb06e52b82cccd2
Rey Bumalay <reyjexter@gmail.com>
2021-10-05 03:30:03 +0800

    Fix a bug on script which non existing cid causes process not to start

--- example/multiaccount/splinterlands-start-all.sh
+++ example/multiaccount/splinterlands-start-all.sh
@@ -8,7 +8,7 @@ echo "Base dir: $base_dir"
 for folder in */ ; do
     account_dir=$base_dir/$folder
     cd $account_dir
-    rm ./cid
+    rm -f ./cid
     echo "Running splinterlands bot on $folder on port $external_port"
     docker run -d -p $external_port:80 -v $account_dir.env:/app/.env --cidfile ./cid splinterlands-bot
     external_port=$(expr $external_port + 1)
